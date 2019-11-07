5f0a533f9d42e1220f86632c214dafbe2d4cb94c
hungtran86 <trannguyenhung011086@gmail.com>
2019-11-07 20:50:55 +0700

    add book instance form

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+const yup = require('yup');
 const BookInstance = require('../models/bookInstance');
+const Book = require('../models/book');
 
 module.exports = {
     getList: async (req, res, next) => {
         try {
-            const listBookInstance = await BookInstance.find({})
+            const bookInstanceList = await BookInstance.find({})
                 .populate('book')
                 .exec();
             res.render('bookInstanceList', {
                 title: 'Book Instance List',
-                bookInstanceList: listBookInstance,
+                bookInstanceList,
             });
         } catch (err) {
             next(err);
@@ -33,11 +35,51 @@ module.exports = {
             next(err);
         }
     },
-    getCreate: (req, res) => {
-        res.send('TODO: book instance create GET');
+    getCreate: async (req, res, next) => {
+        try {
+            const books = await Book.find({}).exec();
+            res.render('bookInstanceForm', {
+                title: 'Book Instance Form',
+                books,
+            });
+        } catch (err) {
+            next(err);
+        }
     },
-    postCreate: (req, res) => {
-        res.send('TODO: book instance create POST');
+    postCreate: async (req, res, next) => {
+        const body = req.body;
+        const books = await Book.find({}).exec();
+
+        try {
+            const schema = yup.object().shape({
+                book: yup.string().required(),
+                imprint: yup.string().required(),
+                status: yup
+                    .string()
+                    .required()
+                    .matches(/available|maintenance|borrowed|reserved/),
+                dueBack: yup.date(),
+            });
+            try {
+                await schema.validate(body);
+            } catch (err) {
+                res.render('bookInstanceForm', {
+                    title: 'Book Instance Form',
+                    books,
+                    bookInstance: body,
+                    errors: err.errors,
+                });
+                return;
+            }
+
+            const bookInstance = await BookInstance.create(body);
+            res.redirect(bookInstance.url);
+        } catch (err) {
+            if (err.name === 'ValidationError') {
+                next(new Error('validation error with Mongo'));
+            }
+            next(err);
+        }
     },
     getDelete: (req, res) => {
         res.send('TODO: book instance delete GET');
