c5d7daa2c284c63d53a64580c1fb7e5684ae3e4b
hungtran86 <trannguyenhung011086@gmail.com>
2019-11-07 18:12:39 +0700

    add book form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const yup = require('yup');
 const Book = require('../models/book');
 const Author = require('../models/author');
 const Genre = require('../models/genre');
@@ -28,8 +29,7 @@ module.exports = {
                 },
             });
         } catch (err) {
-            console.log(err);
-            return res.render('index', {
+            res.render('index', {
                 title: 'Local Library Home',
                 error: err,
             });
@@ -68,11 +68,74 @@ module.exports = {
             next(err);
         }
     },
-    getCreate: (req, res) => {
-        res.send('TODO: book create GET');
+    getCreate: async (req, res, next) => {
+        try {
+            const authors = await Author.find({}).exec();
+            const genres = await Genre.find({}).exec();
+            res.render('bookForm', {
+                title: 'Book Form',
+                authors,
+                genres,
+                book: req.body,
+            });
+        } catch (err) {
+            next(err);
+        }
     },
-    postCreate: (req, res) => {
-        res.send('TODO: book create POST');
+    postCreate: async (req, res, next) => {
+        const body = req.body;
+        try {
+            if (body.genre === undefined) {
+                body.genre = [];
+            }
+
+            const authors = await Author.find({}).exec();
+            const genres = await Genre.find({}).exec();
+
+            const schema = yup.object().shape({
+                title: yup.string().required(),
+                author: yup.string().required(),
+                summary: yup.string().required(),
+                isbn: yup.string().required(),
+                genre: yup
+                    .array()
+                    .min(1)
+                    .of(yup.string().required()),
+            });
+            try {
+                await schema.validate(body);
+            } catch (err) {
+                res.render('bookForm', {
+                    title: 'Book Form',
+                    authors,
+                    genres,
+                    errors: err.errors,
+                });
+                return;
+            }
+
+            // for (let i = 0; i < genres.length; i++) {
+            //     if (body.genre.includes(genres[i]._id)) {
+            //         genres[i].checked = true;
+            //     }
+            // }
+
+            const book = await Book.findOne({
+                title: body.title.trim(),
+            }).exec();
+
+            if (book) {
+                res.redirect(book.url);
+            } else {
+                const newBook = await Book.create(body);
+                res.redirect(newBook.url);
+            }
+        } catch (err) {
+            if (err.name === 'ValidationError') {
+                next(new Error('validation error with Mongo'));
+            }
+            next(err);
+        }
     },
     getDelete: (req, res) => {
         res.send('TODO: book delete GET');
