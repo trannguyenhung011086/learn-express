b6eb9bfc643f49337b9efa955a0dac6e46043bc2
hungtran86 <trannguyenhung011086@gmail.com>
2019-11-08 16:06:39 +0700

    add book instance update

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -121,10 +121,57 @@ module.exports = {
             next(err);
         }
     },
-    getUpdate: (req, res) => {
-        res.send('TODO: book instance update GET');
+    getUpdate: async (req, res, next) => {
+        try {
+            const books = await Book.find({}).exec();
+            const bookInstance = await BookInstance.findById(
+                req.params.id,
+            ).exec();
+            res.render('bookInstanceForm', {
+                title: 'Book Instance Form',
+                books,
+                bookInstance,
+            });
+        } catch (err) {
+            next(err);
+        }
     },
-    postUpdate: (req, res) => {
-        res.send('TODO: book instance update POST');
+    postUpdate: async (req, res, next) => {
+        try {
+            const body = req.body;
+            const books = await Book.find({}).exec();
+
+            const schema = yup.object().shape({
+                book: yup.string().required(),
+                imprint: yup.string().required(),
+                status: yup
+                    .string()
+                    .required()
+                    .matches(/available|maintenance|borrowed|reserved/),
+                dueBack: yup.date(),
+            });
+            try {
+                await schema.validate(body);
+            } catch (err) {
+                res.render('bookInstanceForm', {
+                    title: 'Book Instance Form',
+                    books,
+                    bookInstance: body,
+                    errors: err.errors,
+                });
+                return;
+            }
+
+            const updatedBookInstance = await BookInstance.findByIdAndUpdate(
+                req.params.id,
+                body,
+            ).exec();
+            res.redirect(updatedBookInstance.url);
+        } catch (err) {
+            if (err.name === 'ValidationError') {
+                next(new Error('validation error with Mongo'));
+            }
+            next(err);
+        }
     },
 };
